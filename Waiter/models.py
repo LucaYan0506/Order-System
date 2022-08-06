c8bc6c18cfb7c22831dba4b84574a1ed10a20f32
LucaYan0506 <lucayan0506@gmail.com>
2022-08-06 17:17:43 +0100

    a

--- Waiter/models.py
+++ Waiter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    name = models.CharField(max_length=256)
+    available = models.BooleanField(default=True)
+    occupied = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
