89da1ed9f3cf296666021b2c79cab89a2477202c
LucaYan0506 <lucayan0506@gmail.com>
2022-08-14 15:16:31 +0100

    update

--- Order_System/settings.py
+++ Order_System/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2v155hs*hjolb@j(2jh%td5!*0c!@0k-eklm_0a#&+pm=px6n='
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
