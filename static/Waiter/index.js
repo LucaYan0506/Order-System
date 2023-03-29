0155b8c6ab22fb2e0e698de51f593984a9fa4e49
Luca Yan <lucayan0506>
2023-03-29 23:56:43 +0100

    update send email form

--- static/Waiter/index.js
+++ static/Waiter/index.js
@@ -11,3 +11,26 @@ document.querySelectorAll('#mobile-navbar li').forEach( el => {
         hide_mobile_nav();
     }
 })
+
+function sendEmail(){
+    const data = new FormData(document.querySelector('#contact form'));
+
+    fetch(`/send_message/`,{
+        method: 'POST',
+        header: {  
+            'Content-Type': "multipart/form-data",
+        }, 
+        body: data,
+        credentials: 'same-origin',
+    }).then( (result) => {
+        if (result.status == 200)
+            alert('Message sent successfully, thank you for your message. I will reply as soon as possible (make sure that the email is correct, otherwise I cannot reply)')
+    })
+
+    //clear form
+    document.querySelectorAll('#contact form input')[2].value = ''
+    document.querySelectorAll('#contact form input')[3].value = ''
+    document.querySelector('#contact form textarea').value = ''
+
+    return false;
+}
