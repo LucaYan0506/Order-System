3178cc88a660a0bb65cbcf16d60849a7fbac3d15
Zhong Yi Yan <1418663@stu.tmc.ac.uk>
2023-03-30 09:33:30 +0100

    change setting to production stage and add static root

--- Order_System/settings.py
+++ Order_System/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2v155hs*hjolb@j(2jh%td5!*0c!@0k-eklm_0a#&+pm=px6n='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lucayan3.pythonanywhere.com/']
 
 
 # Application definition
@@ -121,9 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = 'static/'
 
